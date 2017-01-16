753bd8ca7d9d6bad509cb6d3d3cb45114f6b6aa7
Sean Kochel <seankochel@gmail.com>
2017-01-15 22:25:17 -0500

    fixes contestContainer spelling

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import Navbar from './navBar';
-import ContestContainer from './ContestContainer';
+import ContestContainer from './contestContainer';
 
 export default class Home extends React.Component {
 	render() {
